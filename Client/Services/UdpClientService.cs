cea4de6c3d987e50d4eda697dde3ae6d10aecbe9
Yupi <youngperchik2404@gmail.com>
2025-12-01 23:10:30 +0500

    implemented client, but it has bugs

---
+++ Client/Services/UdpClientService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class UdpClientService
+    {
+        private readonly UdpClient udpClient;
+        private readonly IPEndPoint serverEndPoint;
+        private readonly string userName;
+
+        public event Action<string>? MessageReceived;
+
+        public UdpClientService(string serverIp, int port, string userName)
+        {
+            udpClient = new UdpClient();
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+            this.userName = userName;
+        }
+
+
+        public async Task ConnectAsync()
+        {
+            await SendMessage($"HELLO:{userName}");
+            _ = Task.Run(ReceiveMessage);
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await SendMessage($"BYE:{userName}");
+            udpClient.Close();
+        }
+
+
+        public async Task SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await udpClient.SendAsync(data, data.Length, serverEndPoint);
+        }
+
+
+        private async Task ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    var result = await udpClient.ReceiveAsync();
+                    string msg = Encoding.UTF8.GetString(result.Buffer);
+                    //MessageReceived?.Invoke(msg);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Client/View/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Client.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private readonly UdpClientService udpService;
+        private int onlineUsers;
+
+        public ObservableCollection<string> Messages { get; } = new();
+        public ICommand SendMessageCommand { get; }
+        private string messageInput = "";
+        public string MessageInput
+        {
+            get => messageInput;
+            set { messageInput = value; OnPropertyChanged(); }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            udpService = new UdpClientService("192.168.31.146", 8080, Environment.UserName);
+            udpService.MessageReceived += OnMessageReceived;
+            _ = udpService.ConnectAsync();
+
+            SendMessageCommand = new RelayCommand(async _ =>
+            {
+                if (!string.IsNullOrWhiteSpace(MessageInput))
+                {
+                    await udpService.SendMessage(MessageInput);
+                    Messages.Add($"Мое сообщение: {MessageInput}");
+                    MessageInput = "";
+                    OnPropertyChanged(nameof(MessageInput));
+                }
+            });
+        }
+
+
+        public int OnlineUsers
+        {
+            get => onlineUsers;
+            set { onlineUsers = value; OnPropertyChanged(); }
+        }
+
+
+        private void OnMessageReceived(string message)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Messages.Add(message);
+
+                if (message.EndsWith("ONLINE"))
+                {
+                    OnlineUsers++;
+                }
+                else if (message.EndsWith("OFFLINE"))
+                {
+                    OnlineUsers = Math.Max(0, OnlineUsers - 1);
+                }
+            });
+        }
+
+
+        public async Task DisconnectAsync()
+        {
+            await udpService.DisconnectAsync();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+        public void SendMessageClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(MessageInput))
+            {
+                _ = udpService.SendMessage(MessageInput);
+                Messages.Add($"Мое сообщение: {MessageInput}");
+                MessageInput = "";
+                OnPropertyChanged(nameof(MessageInput));
+            }
+        }
+    }
+}
